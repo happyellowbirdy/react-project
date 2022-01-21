50da12908809103dcbdeadfdcc9c7a13a95e73da
Q <qianqianrumail@163.com>
2022-01-21 16:28:55 +0800

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quod
+        voluptate error, qui non sapiente quae facilis explicabo repellat
+        recusandae officia nam perferendis aspernatur ut obcaecati iste, ab
+        maiores labore nesciunt. Dolor enim veniam inventore explicabo itaque
+        minima quis deserunt.
+      </p>
+    </section>
+  );
+};
+
+export default About;
